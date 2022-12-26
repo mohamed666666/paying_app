bf977113958c3cccca009c1965f3b70f24424800
maazaelsharif <85522157+muza65@users.noreply.github.com>
2022-12-26 04:22:20 +0200

    Update admin.java

--- src/paying/admin.java
+++ src/paying/admin.java
@@ -4,15 +4,6 @@ import java.util.ArrayList;
 
 public class admin {
 	
-	ArrayList<refundrequest> reqs=new ArrayList<refundrequest>();
-	public admin(company comp, User user, double fee) {
-		super(comp, user, fee);
-		// TODO Auto-generated constructor stub
-	}
-	refundrequest reqs;
-	User u;
-	transaction t;
-	int num=0;
 	
 
 }
